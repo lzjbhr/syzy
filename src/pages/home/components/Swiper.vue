2d6ba1ed8a4dbd84780292413fb2d361b52b3f06
lzjbhr <343178636@qq.com>
2019-11-13 16:59:48 +0800

    fix

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,10 +2,10 @@
     <div>
     <swiper :option="swiperOption">
     <swiper-slide>
-    <img class=swiper-img; src='http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/c8762210baff5ade1a3b2fe0ab914992.png_750x200_9bbb2f51.png' />
+    <img class="swiper-img" src='http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/c8762210baff5ade1a3b2fe0ab914992.png_750x200_9bbb2f51.png' />
     </swiper-slide>
     <swiper-slide>
-    <img class=swiper-img src='http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/1d8faa9f4d49b77ca1e61e2b478260f7.jpg_750x200_25cf76e2.jpg'/>
+    <img class="swiper-img" src='http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/1d8faa9f4d49b77ca1e61e2b478260f7.jpg_750x200_25cf76e2.jpg'/>
     </swiper-slide>
     <div class="swiper-pagination"  slot="pagination"></div>
   </swiper>
