55a3802f4e83e1dbbc5d9cb0dc787521f87f05e6
lzjbhr <343178636@qq.com>
2019-11-20 15:14:53 +0800

    全新作业

--- src/main.js
+++ src/main.js
@@ -2,17 +2,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-// 导入fastclick解决手机或浏览器可能的300毫秒延迟的问题
 import './assets/styles/reset.css'
-// 重置所有样式
 import './assets/styles/border.css'
-// 解决移动端1像素边框问题
 import './assets/styles/iconfont.css'
 Vue.config.productionTip = false
 fastClick.attach(document.body)
-// 调用fastClick绑定到document.body上
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
