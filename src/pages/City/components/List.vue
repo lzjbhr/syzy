1e28552a9ade2c36dac3fd312c144d68a3bac812
lzjbhr <343178636@qq.com>
2019-12-11 16:00:20 +0800

    12.11

--- src/pages/City/components/List.vue
+++ src/pages/City/components/List.vue
@@ -1,142 +1,92 @@
 <template>
-  <div class="list">
-    <div class="area">
-       <div class="title border-topbottom">当前城市</div>
-       <div class="button-list">
-         <div class="button-wrapper">
-            <div class="button">北京</div>
-         </div>
-       </div>
-    </div>
-    <div class="area">
-       <div class="title border-topbottom">热门城市</div>
-         <div class="button-list">
-         <div class="button-wrapper">
-            <div class="button">北京</div>
-         </div>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
           <div class="button-wrapper">
             <div class="button">北京</div>
-         </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-         </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-         </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-         </div>
-       </div>
-    </div>
-    <div class="area">
-       <div class="title border-topbottom">A</div>
-       <div class="item-list">
-         <div class="item border-bottom">
-           阿拉善
-         </div>
-         <div class="item border-bottom">
-           阿拉善
-         </div>
-         <div class="item border-bottom">
-           阿拉善
-         </div>
-         <div class="item border-bottom">
-           阿拉善
-         </div>
-         <div class="item border-bottom">
-           阿拉善
-         </div>
-       </div>
-              <div class="title border-topbottom">A</div>
-       <div class="item-list">
-         <div class="item border-bottom">
-           阿拉善
-         </div>
-         <div class="item border-bottom">
-           阿拉善
-         </div>
-         <div class="item border-bottom">
-           阿拉善
-         </div>
-         <div class="item border-bottom">
-           阿拉善
-         </div>
-         <div class="item border-bottom">
-           阿拉善
-         </div>
-       </div>
-      <div class="title border-topbottom">B</div>
-       <div class="item-list">
-         <div class="item border-bottom">
-           保定
-         </div>
-         <div class="item border-bottom">
-           包头
-         </div>
-         <div class="item border-bottom">
-           滨州
-         </div>
-         <div class="item border-bottom">
-           宝鸡
-         </div>
-         <div class="item border-bottom">
-           蚌埠
-         </div>
-         <div class="item border-bottom">
-           本溪
-         </div>
-         <div class="item border-bottom">
-           白城亳州
-         </div>
-         <div class="item border-bottom">
-           保山
-         </div>
-         <div class="item border-bottom">
-           巴中
-         </div>
-         <div class="item border-bottom">
-           北海
-         </div>
-       </div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper" 
+              v-for="item of hot" 
+              :key="item.id">
+            <div class="button">{{item.name}}</div>
+          </div>
+        </div>
+      </div>
+      <div class="area" 
+      v-for="(item, key) of cities" 
+      :key='key'
+      >
+        <div class="title border-topbottom">{{key}}</div>
+        <div class="item-list">
+          <div class="item border-bottom" 
+          v-for="innerItem of item"
+          :key="innerItem.id">
+          {{innerItem.name}}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Bscroll from "better-scroll";
 export default {
-  name: 'CityList'
-}
+  name: "CityList",
+  props: {
+    hot: Array,
+    cities: Object
+  },
+  mounted() {
+    this.scroll = new Bscroll(this.$refs.wrapper);
+  }
+};
 </script>
 
 <style lang="stylus" scoped>
-  @import '~styles/varibles.styl'
-  .border-topbottom
-    &:before
-      border-color: #ccc
-    &:after
-      border-color: #ccc
-  .border-bottom
-    &:after
-      border-color: #ccc
+@import '~styles/varibles.styl'
+.border-topbottom
+  &:before
+    border-color #ccc
+  &:after
+    border-color #ccc
+.border-bottom
+  &:before
+    border-color #ccc
+.list
+  position absolute
+  overflow hidden
+  top 1.58rem
+  left 0
+  right 0
+  bottom 0
   .title
-    line-height .44rem
+    line-height 0.44rem
     background #eee
-    padding-left .2rem
+    padding-left 0.2rem
     color #666
-    font-size .26rem
+    font-size 0.26rem
   .button-list
     overflow hidden
-    padding .1rem .6rem .1rem .1rem
+    padding 0.1rem 0.6rem 0.1rem 0.1rem
     .button-wrapper
       float left
       width 33.33%
       .button
         text-align center
-        margin .1rem
-        padding .1rem 0
-        border .02rem solid #ccc
-        border-radius: .08rem
+        margin 0.1rem
+        padding 0.1rem 0
+        border 0.02rem solid #ccc
+        border-radius 0.08rem
   .item-list
     .item
-      line-height .54rem
+      line-height 0.54rem
+      padding-left: .2rem
 </style>
